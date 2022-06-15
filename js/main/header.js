4e46b7b3fd75663f43c8f73b98595ae37bc0b9ad
yousunzoo <102499959+yousunzoo@users.noreply.github.com>
2022-06-15 17:13:57 +0900

    main header 영역 event
    laptop, pc에서의 Gnb 메뉴 이벤트

---
+++ js/main/header.js
@@ -0,0 +1,51 @@
+// header.js
+/** 기능 구현
+ * laptop, pc에서는 gnb menu에 마우스 올릴 시 & focus 이동시 gnb 2dep 전체 보이도록
+ * site map 버튼 눌렀을 때 gnb modal 창 띄우기
+ */
+(function($){
+// 변수
+const wrap = $('#wrap');
+const header = wrap.find('header');
+const gnb = header.find('#gnb');
+const gnbBg = gnb.find('#gnbBg');
+const gnbLi = gnb.find('li');
+const gnb2dep = gnbLi.find('.gnb_2dep');
+const gnbOverlay = header.find('.gnb_overlay_bg');
+let time = 300;
+
+//함수
+const showGnb = function(e){
+  e.preventDefault();
+  gnb2dep.stop().slideDown(time);
+  gnbBg.stop().slideDown(time);
+  gnb.stop().animate({'background-color':'#fff'}, time);
+  gnbOverlay.stop().fadeIn(time);
+}; //showGnb
+
+const hideGnb = function(e){
+  e.preventDefault();
+  gnb2dep.stop().slideUp(time);
+  gnbBg.stop().slideUp(time);
+  gnb.stop().animate({'background-color':"transparent"}, time);
+  gnbOverlay.stop().fadeOut(time);
+}; //hideGnb
+
+// event
+// mouseenter + focus
+gnbLi.on('mouseenter', showGnb);
+gnbLi.children('a').on('focus', showGnb);
+
+// mouseout + blur
+gnbLi.on('mouseout',hideGnb);
+gnbLi.children('a').on('blur', hideGnb);
+
+
+
+
+
+
+
+
+
+})(jQuery);
