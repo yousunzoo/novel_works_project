bfb67d6353831fd6fc2aed5f70a614d5c4423330
yousunzoo <102499959+yousunzoo@users.noreply.github.com>
2022-06-30 17:04:26 +0900

    Update product_import_data.js
    불필요한 부분 삭제

--- js/sub/product_import_data.js
+++ js/sub/product_import_data.js
@@ -17,19 +17,13 @@
         header.load(baseUrl +importPage[0], function(){
           let headScript = `<script src="../js/page/header.js" class="head_script"></script>`
           let headScriptM = `<script src="../js/page/m_header.js" class="head_script_m"></script>`
-          let developScript = `<script src="../js/sub/development_tab.js" class="develop_tab"></script>`;
-          let developScriptM = `<script src="../js/sub/development_tab_m.js" class="develop_tab_m"></script>`
-    
+
           if(deviceCk === 'smartphone' || deviceCk === 'tablet'){
             body.append(headScriptM);
             body.remove($('.head_script'));
-            body.append(developScriptM);
-            body.remove($('.develop_tab'));
           } else {
             body.append(headScript);
             body.remove($('.head_script_m'));
-            body.append(developScript);
-            body.remove($('.develop_tab_m'));
           };
         });
       
